8ee8c35f8ae690db723c05118404a392f65aaa8e
Eunice Park <28845173+eunipa@users.noreply.github.com>
2018-07-19 02:14:50 -0700

    Install and use Bootstrap typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,11 @@
 import Typography from "typography";
 
-const typography = new Typography({ baseFontSize: "18px" });
+// Typography.js themes are simple Javascript objects (Source Typography readme).
+
+import bootstrapTheme from "typography-theme-bootstrap";
+// import lawtonTheme from "typography-theme-lawton";
+
+const typography = new Typography(bootstrapTheme);
+// const typography = new Typography(lawtonTheme);
 
 export default typography;
